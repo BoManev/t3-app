a016c5e49987bef35e9a7e34085063780ec67f5e
BoManev <bomanev@pm.me>
2023-05-31 06:12:25 -0400

    style up page

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,8 +1,51 @@
+import { clerkClient } from "@clerk/nextjs";
+import { type User } from "@clerk/nextjs/dist/types/server";
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
+const filterUserForClient = (user: User) => {
+  let username = "";
+  if (user.username) {
+    username = user.username;
+  } else if (user.firstName && user.lastName) {
+    username = `${user.firstName} ${user.lastName}`;
+  }
+
+  return {
+    id: user.id,
+    username,
+    profileImageUrl: user.profileImageUrl,
+  };
+};
 export const postsRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.post.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.prisma.post.findMany({
+      take: 100,
+    });
+
+    const users = (
+      await clerkClient.users.getUserList({
+        userId: posts.map((post) => post.authorId),
+        limit: 100,
+      })
+    ).map(filterUserForClient);
+
+    console.log(users);
+
+    return posts.map((post) => {
+      const author = users.find((user) => user.id === post.authorId);
+      if (!author || !author.username) {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Author Not Found!",
+        });
+      }
+
+      return {
+        post,
+        author: { ...author, username: author.username },
+      };
+    });
   }),
 });
